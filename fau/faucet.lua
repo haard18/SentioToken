4a0b573e6ded571c08225047209b97be860d66f7
haard18 <haardsolanki.itm@gmail.com>
2024-10-28 02:10:49 +0530

    Refactor faucet.lua to handle airdrop requests for SENTIO tokens
    This commit refactors the faucet.lua file to handle airdrop requests for SENTIO tokens. It introduces a check for the recipient address and assigns it to the 'address' variable. If the 'Recipient' field is not present in the message, it assigns the 'From' field to the 'address' variable. The commit also updates the 'Recipient' field in the outgoing message to use the 'address' variable instead of directly using 'msg.From'.

--- fau/faucet.lua
+++ fau/faucet.lua
@@ -6,7 +6,12 @@ AIRDROPPED_QUANTITY = "10000000000000"
 
 Handlers.add('airdrop-tokens', Handlers.utils.hasMatchingTag('Action', 'tSENTI-drop'), function(msg)
     local currentHeight = tonumber(msg['Block-Height'])
-    local address = msg.From
+    local address
+    if not msg.Recipient then
+      address = msg.From
+    else
+      address = msg.Recipient
+    end
 
     if AirdropedAtBlock[address] then
         assert(currentHeight - AirdropedAtBlock[address] > 1, "Please wait for some time before requesting airdrop again!")
@@ -18,7 +23,7 @@ Handlers.add('airdrop-tokens', Handlers.utils.hasMatchingTag('Action', 'tSENTI-d
 
     ao.send({
       Target = msg.Target,
-      Recipient = msg.From,
+      Recipient = address,
       Action = 'Transfer',
       Quantity = AIRDROPPED_QUANTITY,
     })
